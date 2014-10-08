a9a3d6a994ccb55ed6c200a9a6dd29c1a3a14ce1
Jack Conger <mail@jpconger.com>
2014-10-08 13:44:21 -0700

    Added install.sh

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# needs sudo privileges; look at code first though!
+
+if [[ $# -ne 0 ]]; then
+	if [[ $1 = '-r' ]]; then
+		unlink /bin/rmpp
+		unlink /bin/unrm
+		unlink /bin/rmdirpp
+		unlink /bin/emptytrash
+		exit 0
+	fi
+fi
+
+ln -s $(pwd)/rmpp /bin/rmpp
+ln -s $(pwd)/unrm /bin/unrm
+ln -s $(pwd)/rmdirpp /bin/rmdirpp
+ln -s $(pwd)/emptytrash /bin/emptytrash
